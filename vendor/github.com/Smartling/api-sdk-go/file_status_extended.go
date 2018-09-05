e4c10b58ee80866e27a474ef0e1cecf738af5248
Daniel Williams <daniel.williams@99designs.com>
2018-09-05 10:11:01 +1000

    Add support to vendored library for extended status endpoint
    Also add some helper methods

---
+++ vendor/github.com/Smartling/api-sdk-go/file_status_extended.go
@@ -0,0 +1,28 @@
+package smartling
+
+// FileStatusExtended describes file translation status obtained by GetFileStatusExtended
+// method.
+type FileStatusExtended struct {
+	File
+
+	AuthorizedStringCount int
+	AuthorizedWordCount   int
+	CompletedStringCount  int
+	CompletedWordCount    int
+	ExcludedStringCount   int
+	ExcludedWordCount     int
+	TotalStringCount      int
+	TotalWordCount        int
+}
+
+func (fs FileStatusExtended) AwaitingAuthorizationStringCount() int {
+	return fs.TotalStringCount - fs.AuthorizedStringCount
+}
+
+func (fs FileStatusExtended) InProgressStringCount() int {
+	return fs.NotCompletedStringCount() - fs.AwaitingAuthorizationStringCount()
+}
+
+func (fs FileStatusExtended) NotCompletedStringCount() int {
+	return fs.TotalStringCount - fs.CompletedStringCount
+}
